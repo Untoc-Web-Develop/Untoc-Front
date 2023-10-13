625ad59f34ac2e90d690038dddf73f6e5771bfc3
jasper200207 <jasper12394@gmail.com>
2023-10-13 19:34:08 +0900

    feat : redux 적용

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'css/index.css';
 import App from 'page/App';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from 'store';
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+);
