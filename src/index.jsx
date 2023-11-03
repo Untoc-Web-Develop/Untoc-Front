2fe69f33a8a7c16d6c78eff0b08fdf4777b783a2
Kim <jasper12394@gmail.com>
2023-11-04 02:25:58 +0900

    style : index.jsx import order 적용
    #13

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
+import ReactDOM from 'react-dom/client';
 import 'css/index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppRouter from 'routes/AppRouter';
