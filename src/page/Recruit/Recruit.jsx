a5fb94217ae3aae6194448ea42fe23963bca331a
daehee <gkseogml4959@gmail.com>
2023-11-07 18:25:47 +0900

    feat : 신입 회원 모집 페이지 레이아웃 추가
    - 전체 페이지 화면을 세로로 2분할함
    - 모든 글자를 하드코딩하여 이후 변경이 필요함
    - #2

--- src/page/Recruit/Recruit.jsx
+++ src/page/Recruit/Recruit.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import RecruitDescription from './RecruitDescription';
+import RecruitForm from './RecruitForm';
+
 const Recruit = () => {
-  return <div />;
+  return (
+    <div className="h-screen w-screen flex justify-center align-center bg-[#F8F8F9]">
+      <div className="h-full w-[1200px] flex justify-center align-center">
+        <div className="h-full w-full grid grid-cols-2 gap-4 grid-rows-1">
+          <div className="h-full w-full flex">
+            <div className="h-5/6 w-full self-center">
+              <RecruitDescription />
+            </div>
+          </div>
+          <div className="h-full w-full flex">
+            <div className="h-5/6 w-full self-center">
+              <RecruitForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Recruit;
