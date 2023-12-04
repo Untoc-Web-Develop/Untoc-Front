23e4ad046b14ff9eecd174084536f3ce6a986eee
daehee <gkseogml4959@gmail.com>
2023-12-04 22:05:50 +0900

    feat: 기본 모달
    -useRef를 사용한 모달 바깥 클릭시 모달 닫힘

---
+++ src/components/Modals/Modal.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+
+import PropTypes from 'prop-types';
+
+const Modal = ({ children, isOpen, setIsOpen }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [modalRef, setIsOpen]);
+
+  if (!isOpen) return null;
+  return (
+    <div className="z-[999] h-screen w-screen fixed top-0 left-0 flex flex-col justify-center items-center bg-opacity-50 bg-black">
+      <div ref={modalRef} className="h-72 w-[30rem] bg-white shadow-xl">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  setIsOpen: PropTypes.func.isRequired,
+};
+
+export default Modal;
