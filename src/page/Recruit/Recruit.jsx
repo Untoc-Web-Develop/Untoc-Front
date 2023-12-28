136ce145b5989466309db108bbbc652b8a050209
daehee <gkseogml4959@gmail.com>
2023-12-28 13:16:37 +0900

    style : tailwindcss 정렬

--- src/page/Recruit/Recruit.jsx
+++ src/page/Recruit/Recruit.jsx
@@ -5,16 +5,16 @@ import RecruitForm from './RecruitForm';
 
 const Recruit = () => {
   return (
-    <div className="flex justify-center w-screen overflow-x-hidden h-content align-center bg-grayLight">
-      <div className="flex flex-col justify-center w-4/5 h-fit align-center">
-        <div className="grid w-full h-full grid-cols-2 grid-rows-1 gap-4">
-          <div className="flex items-start w-full h-full mt-16">
-            <div className="sticky w-full top-16 h-fit">
+    <div className="align-center flex h-content w-screen justify-center overflow-x-hidden bg-grayLight">
+      <div className="align-center flex h-fit w-4/5 flex-col justify-center">
+        <div className="grid h-full w-full grid-cols-2 grid-rows-1 gap-4">
+          <div className="mt-16 flex h-full w-full items-start">
+            <div className="sticky top-16 h-fit w-full">
               <RecruitDescription />
             </div>
           </div>
-          <div className="flex items-start w-full h-full mt-16">
-            <div className="w-full h-5/6">
+          <div className="mt-16 flex h-full w-full items-start">
+            <div className="h-5/6 w-full">
               <RecruitForm />
             </div>
           </div>
