8995ba74f37544513f0f24b31a541cbf6ee3408e
daehee <gkseogml4959@gmail.com>
2023-12-29 16:26:08 +0900

    feat : admin 권한 적용

--- src/page/Admin/Layouts/AdminLayout.jsx
+++ src/page/Admin/Layouts/AdminLayout.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import useRoleCheck from 'hooks/useRoleCheck';
+
 import AdminTab from '../components/AdminTab/AdminTab';
 
 const AdminLayout = () => {
+  const { OnlyLogin } = useRoleCheck('admin');
+
   return (
     <div className="flex h-content w-full justify-center">
-      <div className="h-full w-[800px] lg:w-4/5">
-        <div className="flex h-1/6 items-center">
-          <AdminTab />
+      <OnlyLogin>
+        <div className="h-full w-[800px] lg:w-4/5">
+          <div className="flex h-1/6 items-center">
+            <AdminTab />
+          </div>
+          <div className="h-5-6">
+            <Outlet />
+          </div>
         </div>
-        <div className="h-5-6">
-          <Outlet />
-        </div>
-      </div>
+      </OnlyLogin>
     </div>
   );
 };
