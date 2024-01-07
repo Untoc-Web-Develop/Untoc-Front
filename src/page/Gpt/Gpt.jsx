dfb8e7978df97f9f2b5b04d36dccc8aa40f78c28
parkqjunseo <junsns7@naver.com>
2024-01-07 17:27:14 +0900

    feat : GPT 페이지 레이아웃 추가
     -#63

---
+++ src/page/Gpt/Gpt.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import DelImg from 'asset/gpt/deleteImg.png';
+import GptImg from 'asset/gpt/gptImg.png';
+import SendImg from 'asset/gpt/sendImg.png';
+import YouImg from 'asset/gpt/youImg.png';
+
+const Gpt = () => {
+  return (
+    <div className="flex h-content overflow-hidden">
+      {/* 사이드바 */}
+      <div className="flex-shrink-0 w-[16rem] bg-grayLight overflow-y-auto overflow-x-hidden">
+        <div className="flex h-full min-h-0 flex-col p-3">
+          <div className="bg-grayDark50 p-2 mb-2 rounded-xl font-bold--">+ New Chat</div>
+          <div className="border-t-2 border-gray-400 mb-2" />
+          {/* 대화 기록 예시 */}
+          <div className="p-2">
+            <h3 className="text-xl font mb-2">오늘</h3>
+            <ol>
+              <li>
+                <div className="flex bg-grayDark50 mb-2 p-2 rounded-xl justify-between items-center">
+                  오늘 점메추 ....
+                  <img src={DelImg} alt="Delete" className="w-5 h-5" />
+                </div>
+              </li>
+              <li>
+                <div className="flex bg-grayDark50 mb-2 p-2 rounded-xl justify-between items-center">
+                  대충 질문 요약 ....
+                  <img src={DelImg} alt="Delete" className="w-5 h-5" />
+                </div>
+              </li>
+            </ol>
+          </div>
+
+          <div className="p-2">
+            <h3 className="text-xl mb-2">3일전</h3>
+            <ol>
+              <li>
+                <div className="flex bg-grayDark50 mb-2 p-2 rounded-xl justify-between items-center">
+                  대충 질문 요약 ....
+                  <img src={DelImg} alt="Delete" className="w-5 h-5" />
+                </div>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+
+      {/* 메인 화면 */}
+      <div className="flex-1 flex justify-center">
+        <div className="flex h-full flex-col p-2 w-[50vw]">
+          {/* 채팅내역 */}
+          <div className="p-4 overflow-auto h-full">
+            {/* 예시 채팅 */}
+            <div className="p-2 flex items-center rounded">
+              <img src={GptImg} alt="Chat GPT" className="mr-7 w-12 h-12" />
+              <div className="bg-grayDark50 p-4 mt-10 rounded-xl" style={{ borderRadius: '0 1rem 1rem 1rem' }}>
+                <p className="text-base">안녕하세요. 언톡 Chat GPT입니다. ~~~~~</p>
+              </div>
+            </div>
+
+            <div className="p-2 flex justify-end rounded">
+              <div className="bg-grayDark50 p-4 mt-10 rounded-xl" style={{ borderRadius: '1rem 0 1rem 1rem' }}>
+                <p className="text-base">질문 ~~~~</p>
+              </div>
+              <img src={YouImg} alt="You" className="ml-7 w-12 h-12" />
+            </div>
+
+            <div className="p-2 flex items-center rounded">
+              <img src={GptImg} alt="Chat GPT" className="mr-7 w-12 h-12" />
+              <div className="bg-grayDark50 p-4 mt-10 rounded-xl" style={{ borderRadius: '0 1rem 1rem 1rem' }}>
+                <p className="text-base">답변 ~~~~</p>
+              </div>
+            </div>
+          </div>
+          {/* 입력창 */}
+          <div className="h-16 flex items-center">
+            <div className="bg-grayDark50 flex items-center px-2 rounded-lg flex-grow">
+              <textarea className="flex-1 bg-grayDark50 p-2" placeholder="Send Message" />
+              <button type="submit" className="ml-2">
+                <img src={SendImg} alt="Send" className="w-5 h-5" />
+              </button>
+            </div>
+            <div className="bg-grayDark50 ml-2 px-5 py-2 rounded font-bold">4 / 5</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gpt;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import MainLayout from 'components/Layouts/MainLayout';
 import Board from 'page/Board/Board';
+import Gpt from 'page/Gpt/Gpt';
 import Login from 'page/Login/Login';
 import Main from 'page/Main/Main';
 import NotFound from 'page/NotFound/NotFound';
@@ -20,6 +21,7 @@ const AppRouter = () => {
         <Route path="profile" element={<Profile />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="gpt" element={<Gpt />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
