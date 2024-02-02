aa45b5fc390e2f4fb0185f2f2f0d201aab3c3aa9
Daehee Han <gkseogml4959@gmail.com>
2024-02-02 15:56:00 +0900

    feat : 관리자 관리 페이지 임시 데이터

---
+++ src/page/Admin/AdminManageRole/userList.js
@@ -0,0 +1,12 @@
+const users = [
+  { id: 1, name: '김대욱', role: '회장', role1: true, role2: true, role3: true, role4: true },
+  { id: 2, name: '정원석', role: '부회장', role1: false, role2: true, role3: false, role4: true },
+  { id: 3, name: '한대희', role: '집행부', role1: true, role2: false, role3: true, role4: true },
+  { id: 4, name: '한대희', role: '회원', role1: false, role2: false, role3: false, role4: false },
+  { id: 5, name: '한대희', role: '회원', role1: false, role2: false, role3: false, role4: false },
+  { id: 6, name: '한대희', role: '회원', role1: false, role2: false, role3: false, role4: false },
+  { id: 7, name: '한대희', role: '회원', role1: false, role2: false, role3: false, role4: false },
+  { id: 8, name: '한대희', role: '회원', role1: false, role2: false, role3: false, role4: false },
+];
+
+export default users;
