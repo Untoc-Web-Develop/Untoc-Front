3c1da5861b79ad75fde9f27f6d94234992b86f5e
jimini1026 <2004imjimin1@gmail.com>
2024-02-13 22:03:35 +0900

    Design : 공통 컴포넌트 사용
    - 공통 컴포넌트 사용
    - 컴포넌트 크기에 맞게 글자 크기 확대
    - #72

--- src/page/TeamList/TeamList.jsx
+++ src/page/TeamList/TeamList.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 
-import { CardActionArea } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import teamListImg from 'asset/teamlist/teamListImg.png';
+import TeamInfoCard from 'components/TeamInfoCard/TeamInfoCard';
 import TeamListHeader from 'components/TeamListHeader/TeamListHeader';
-
-const Team = () => {
-  return (
-    <Card sx={{ width: '15rem' }} elevation={3}>
-      <CardActionArea>
-        <CardMedia component="img" image={teamListImg} alt="TeamImg" sx={{ height: 100 }} />
-        <CardContent sx={{ height: 75 }}>
-          <Typography gutterBottom component="div" sx={{ lineHeight: '1.3', fontSize: '1rem' }}>
-            LEAD
-          </Typography>
-          <Typography className="text-placeHolder" sx={{ lineHeight: '1.2', fontSize: '0.6rem' }}>
-            김세론 김세론2 김세론3 김세론4 김세론5 <br />
-            김세론6 김세론7
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
-};
 
 const TeamList = () => {
   return (
@@ -34,35 +10,36 @@ const TeamList = () => {
         <TeamListHeader content="teamlist" isTeamed={false} />
       </div>
       <div className="flex items-center justify-center">
-        <div className="flex w-[70rem] flex-col gap-10">
-          <div className="h-[30rem] rounded-[2rem] bg-white">
+        <div className="flex w-[80rem] flex-col gap-10">
+          <div className="h-[38rem] rounded-[2rem] bg-white">
             <hr className="border border-yellowPoint" />
             <div className="mx-10 mt-3">
-              <div className="mx-2 my-5 text-2xl font-bold text-yellowPoint">2023학년도 2학기</div>
+              <div className="mx-2 my-5 text-3xl font-bold text-yellowPoint">2023학년도 2학기</div>
               <div className="my-5 flex items-center justify-center">
                 <div className="grid grid-cols-4 grid-rows-2 gap-5">
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
                 </div>
               </div>
             </div>
           </div>
-          <div className="h-[30rem] rounded-[2rem] bg-white">
+          <div className="h-[38rem] rounded-[2rem] bg-white">
             <hr className="border border-placeHolder" />
             <div className="mx-10 mt-3">
-              <div className="mx-2 my-5 text-2xl font-bold text-placeHolder">2023학년도 1학기</div>
+              <div className="mx-2 my-5 text-3xl font-bold text-placeHolder">2023학년도 1학기</div>
               <div className="my-5 flex items-center justify-center">
                 <div className="grid grid-cols-4 grid-rows-2 gap-5">
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
-                  <Team />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
+                  <TeamInfoCard title="Lead" content="김세론 김세론2 김세론3 김세론4 김세론5 김세론6 김세론7" />
                 </div>
               </div>
             </div>
