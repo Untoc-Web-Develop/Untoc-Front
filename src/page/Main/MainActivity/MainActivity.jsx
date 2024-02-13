bd2a7e14c6f705a30b686b913f691f4b24193eb4
munkyeong <horse6953@naver.com>
2024-02-13 17:39:42 +0900

    feat : padding 간격 수정

--- src/page/Main/MainActivity/MainActivity.jsx
+++ src/page/Main/MainActivity/MainActivity.jsx
@@ -26,26 +26,26 @@ const MainActivity = () => {
   }, [animate]);
 
   return (
-    <div className="pb-10 pt-52 text-grayDark">
+    <div className="py-40  text-grayDark">
       <div className=" mx-auto mb-24 w-fit space-y-3 text-center">
         <div className="text-3xl ">지난 우리 활동은,</div>
         <div>2023학년 1학기부터 진행된 UntoC의 활동을 소개합니다!</div>
       </div>
-      <div className=" flex h-64 items-center overflow-hidden">
+      <div className=" flex h-64 items-center overflow-hidden ">
         <ul className="relative flex flex-nowrap" onMouseEnter={onStop} onMouseLeave={onStart}>
           <div className="absolute left-0 flex animate-InfinitySlider flex-nowrap items-center gap-8 pr-8">
             {titles.map((items) => (
-              <TeamInfoCard key={items.title} title={items.title} content={items.content} IsCaptionRequired />
+              <TeamInfoCard key={items.title} title={items.title} content={items.content} />
             ))}
           </div>
           <div className="flex animate-InfinitySlider_copy flex-nowrap items-center gap-8 pr-8">
             {titles.map((items) => (
-              <TeamInfoCard key={items.title} title={items.title} content={items.content} IsCaptionRequired />
+              <TeamInfoCard key={items.title} title={items.title} content={items.content} />
             ))}
           </div>
         </ul>
       </div>
-      <div className="!mt-80 flex flex-col items-center justify-center ">
+      <div className=" flex flex-col items-center justify-center ">
         <Button variant="contained" className="!mt-10 !w-52 !bg-yellowPoint !font-semibold !shadow-none ">
           전체보기
         </Button>
