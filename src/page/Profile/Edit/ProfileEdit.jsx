4d394940bf444730668b5efc74e05ea0638db219
jimini1026 <2004imjimin1@gmail.com>
2024-02-25 17:38:45 +0900

    design : 프로필 수정 페이지 반응형
    - 반응형 수정
    - #62

--- src/page/Profile/Edit/ProfileEdit.jsx
+++ src/page/Profile/Edit/ProfileEdit.jsx
@@ -105,41 +105,41 @@ const ProfileEdit = () => {
   }, [values1, values2]);
 
   return (
-    <div className="min-h-screen bg-grayLight pt-8">
+    <div className="min-h-screen w-full bg-grayLight pt-8">
       <div className="mb-[3rem] flex h-[3rem] flex-col items-center justify-center bg-white">
         <div className="text-sm font-semibold text-grayDark ">Edit and Update Your Profile!</div>
       </div>
       <hr className="mx-[12rem] my-3" />
-      <div className="mx-[12rem]">
-        <div className="flex h-80">
-          <div className="w-[25rem] text-lg font-semibold text-grayDark">Personal Info</div>
-          <form className="flex-grow" onSubmit={handleSubmit1}>
-            <div className="my-auto rounded-b-lg shadow-lg">
-              <div className="grid h-60 grid-cols-2 grid-rows-3 items-center rounded-t-lg bg-white text-sm">
-                <div className="mx-auto">
+      <div className="mx-auto w-3/4">
+        <div className="flex w-full">
+          <div className="w-1/4 text-lg font-semibold text-grayDark">| Personal Info</div>
+          <form className="w-3/4" onSubmit={handleSubmit1}>
+            <div className="rounded-b-lg shadow-lg">
+              <div className="grid h-60 w-full grid-cols-2 grid-rows-3 items-center gap-5 rounded-t-lg bg-white text-sm">
+                <div className="mx-5">
                   <div className="mb-1 font-semibold text-grayDark">이름</div>
                   <input
                     id="name"
                     type="text"
-                    className="h-8 w-[20rem] rounded-lg border border-placeHolder pl-3"
+                    className="h-8 w-full rounded-lg border border-placeHolder pl-3"
                     value={name}
                     onChange={onChange}
                     placeholder="김세론"
                   />
                 </div>
                 <br />
-                <div className="mx-auto">
+                <div className="mx-5">
                   <div className="mb-1 font-semibold text-grayDark">Email</div>
                   <input
                     id="email"
                     type="email"
-                    className="h-8 w-[20rem] rounded-lg border border-placeHolder pl-3"
+                    className="h-8 w-full rounded-lg border border-placeHolder pl-3"
                     value={email}
                     onChange={onChange}
                     placeholder="UntoC@gmail.com"
                   />
                 </div>
-                <div className="mx-auto">
+                <div className="mx-5">
                   <div className="mb-1 font-semibold text-grayDark">
                     전화번호
                     <label className="text-xs text-placeHolder" htmlFor="hidePhoneNumber">
@@ -155,37 +155,37 @@ const ProfileEdit = () => {
                   <input
                     id="tel"
                     type="tel"
-                    className="h-8 w-[20rem] rounded-lg border border-placeHolder pl-3"
+                    className="h-8 w-full rounded-lg border border-placeHolder pl-3"
                     value={tel !== 'hidden' ? tel : ''}
                     onChange={onChange}
                     placeholder={tel !== 'hidden' ? '010-1234-5678' : ''}
                     disabled={tel === 'hidden'}
                   />
                 </div>
-                <div className="mx-auto">
+                <div className="mx-5">
                   <div className="mb-1 font-semibold text-grayDark">링크 URL (블로그, github 등)</div>
                   <input
                     id="link1"
                     type="url"
-                    className="h-8 w-[20rem] rounded-lg border border-placeHolder pl-3"
+                    className="h-8 w-full rounded-lg border border-placeHolder pl-3"
                     value={link1}
                     onChange={onChange}
                     placeholder="http://untoC-github"
                   />
                 </div>
-                <div className="mx-auto">
+                <div className="mx-5">
                   <div className="mb-1 font-semibold text-grayDark">링크 URL (블로그, github 등)</div>
                   <input
                     id="link2"
                     type="url"
-                    className="h-8 w-[20rem] rounded-lg border border-placeHolder pl-3"
+                    className="h-8 w-full rounded-lg border border-placeHolder pl-3"
                     value={link2}
                     onChange={onChange}
                     placeholder="http://UntoC-blog.co.kr"
                   />
                 </div>
               </div>
-              <div className="flex h-10 items-center justify-end">
+              <div className="flex h-10 items-center justify-end ">
                 <button
                   className="mr-7 h-7 w-[5.5rem] rounded-lg border-2 border-yellowPoint bg-white text-sm font-semibold text-yellowPoint"
                   type="submit"
@@ -197,12 +197,12 @@ const ProfileEdit = () => {
           </form>
         </div>
         <hr className="my-3" />
-        <div className="flex">
-          <div className="w-[25rem] text-lg font-semibold text-grayDark">Profile</div>
-          <form className="flex-grow" onSubmit={handleSubmit2}>
+        <div className="flex w-full">
+          <div className="w-1/4 text-lg font-semibold text-grayDark">| Profile</div>
+          <form className="w-3/4" onSubmit={handleSubmit2}>
             <div className="rounded-b-lg shadow-lg">
-              <div className="flex h-40 rounded-t-lg bg-white">
-                <div className="mt-7 flex w-[11rem] flex-col items-center justify-center">
+              <div className="h-45 flex w-full justify-between rounded-t-lg bg-white">
+                <div className="mt-7 flex w-[11rem] flex-col items-center justify-center ">
                   <img className="m-3 h-20 w-20 rounded-full" src={profileImg} alt="현재 이미지" />
                   <label htmlFor="profileImg">
                     <div className="flex h-7 w-[7rem] cursor-pointer items-center justify-center bg-grayLight text-xs text-grayDark">
@@ -243,15 +243,15 @@ const ProfileEdit = () => {
                     휴면
                   </label>
                 </div>
-                <div className="ml-5 flex-grow py-9">
+                <div className="mx-5 w-fit py-9">
                   <div className="mb-1 text-sm font-semibold text-grayDark">
                     <li>한줄 소개 작성</li>
                   </div>
                   <textarea
                     id="selfIntroduction"
-                    className="rounded-lg border border-placeHolder px-3 py-3 text-xs"
-                    cols="60"
-                    rows="4"
+                    className="resize-none rounded-lg border border-placeHolder px-3 py-3 text-xs"
+                    cols="63"
+                    rows="6"
                     value={selfIntroduction}
                     maxLength={50}
                     onChange={onChange}
