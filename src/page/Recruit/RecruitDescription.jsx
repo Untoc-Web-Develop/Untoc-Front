cbfc7dba7857640e0aec319368d40843638b06a3
daehee <gkseogml4959@gmail.com>
2024-02-25 16:17:34 +0900

    feat : 신입회원 모집 설명 api

--- src/page/Recruit/RecruitDescription.jsx
+++ src/page/Recruit/RecruitDescription.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { useGetApplySettingQuery } from 'api/applyApi';
 import RecruitImg from 'asset/recruit/recruitImg.png';
+import useRoleCheck from 'hooks/useRoleCheck';
 
 const RecruitDescription = () => {
+  const { isLogin } = useRoleCheck('admin');
+  const { data: applySetting, isLoading } = useGetApplySettingQuery();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!applySetting && !isLogin && !isLoading) {
+      alert('모집 설정이 없습니다. 관리자에게 문의해주세요.');
+      navigate('/', { replace: true });
+    }
+  }, [applySetting, isLogin, navigate, isLoading]);
+
+  const dateToString = (dateString) => {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+  };
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-start px-12 text-sm font-normal">
       <div className="mb-10 h-2/5">
-        <h1 className="mb-16 text-4xl">UntoC 16기 신입회원 모집</h1>
-        <h2 className="mb-10 text-base text-yellowPoint">모집기간 : 2023.08.15 ~ 2023.08.31</h2>
-        <p>
-          지원을 하실 때, 학번 이름, 전화번호를 정확하게 기재해주세요. <br />
-          지원 기한 종료 이후에는 해당 데이터를 안전하게 폐기할 예정이오니 <br />
-          이점 유의바랍니다.
-          <br />
-          <br />
-          신입회원 모집 관련 문의는 ___@___ 으로 연락주시거나 <br />
-          UntoC 카카오톡 오픈 채팅방으로 문의 부탁드립니다. <br />
-          감사합니다.
-        </p>
+        <h1 className="mb-16 text-4xl">UntoC {applySetting?.data.generation}기 신입회원 모집</h1>
+        <h2 className="mb-10 text-base text-yellowPoint">
+          모집기간 : {dateToString(applySetting?.data.openAt)} ~ {dateToString(applySetting?.data.closeAt)}
+        </h2>
+        <p>{applySetting?.data.content || '모집 설명이 없습니다.'}</p>
       </div>
       <div className="h-2/5">
         <img src={RecruitImg} alt="recruitImg" />
