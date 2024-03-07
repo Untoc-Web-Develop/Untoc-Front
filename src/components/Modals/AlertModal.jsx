165288056e427d4aa9eefce22a0c27d20fed0b08
Daehee Han <gkseogml4959@gmail.com>
2024-03-07 15:18:39 +0900

    feat: 모달 사이즈 추가

--- src/components/Modals/AlertModal.jsx
+++ src/components/Modals/AlertModal.jsx
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types';
 
 import Modal from './Modal';
 
-const AlertModal = ({ children, isOpen, setIsOpen, onClose, outsideClose, closeBtnText }) => {
+const AlertModal = ({ children, isOpen, setIsOpen, onClose, outsideClose, closeBtnText, size }) => {
   const handleClose = () => {
     onClose();
     setIsOpen(false);
   };
 
   return (
-    <Modal isOpen={isOpen} setIsOpen={setIsOpen} onClose={onClose} outsideClose={outsideClose}>
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} onClose={onClose} outsideClose={outsideClose} size={size}>
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <div className="text-md flex h-3/4 w-full items-center justify-center font-bold text-grayDark">{children}</div>
-        <div className="flex h-1/4 w-full items-center justify-center">
+        <div className="text-md flex h-5/6 w-full items-center justify-center font-bold text-grayDark">{children}</div>
+        <div className="flex h-1/6 w-full items-center justify-center">
           <button
-            className="text-md h-1/2 w-1/5 rounded-lg border border-yellowPoint font-bold text-yellowPoint"
+            className="text-md h-8 w-1/5 rounded-lg border border-yellowPoint font-bold text-yellowPoint"
             type="button"
             onClick={handleClose}
           >
@@ -35,12 +35,14 @@ AlertModal.propTypes = {
   onClose: PropTypes.func,
   outsideClose: PropTypes.bool,
   closeBtnText: PropTypes.string,
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
 };
 
 AlertModal.defaultProps = {
   outsideClose: false,
   onClose: () => {},
   closeBtnText: '확인',
+  size: 'sm',
 };
 
 export default AlertModal;
